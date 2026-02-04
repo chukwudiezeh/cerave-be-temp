af86ebcee1547087fdb93d18b56dd91fce93acd9
Chukwudi Ezeh <chukwudi.ezeh@konga.com>
2026-02-04 09:37:16 +0100

    add cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({origin: "*"});
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new ExceptionsFilter());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
